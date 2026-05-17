f07cac9f5421e78a60a6d009360d77ceb6c97da4
maria-khan-khushk <153493453+maria-khan-khushk@users.noreply.github.com>
2026-05-17 22:11:37 +0500

    updated with new deployment links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
         <div className="flex gap-6">
           <a href="https://github.com/UmerCodes19/Trace" target="_blank" rel="noreferrer" className="text-xs text-fg-muted hover:text-jade-400 transition-colors">GitHub</a>
           <a href="https://trace-self.vercel.app" target="_blank" rel="noreferrer" className="text-xs text-fg-muted hover:text-jade-400 transition-colors">Dashboard</a>
-          <a href="https://trace-app-chi.vercel.app" target="_blank" rel="noreferrer" className="text-xs text-fg-muted hover:text-jade-400 transition-colors">Web App</a>
+          <a href=" https://trace-app-xi.vercel.app" target="_blank" rel="noreferrer" className="text-xs text-fg-muted hover:text-jade-400 transition-colors">Web App</a>
         </div>
       </div>
     </footer>
